68974b811d740544e53ad3678d8e2c4534024873
Huego <hugozaanen@hotmail.com>
2018-08-21 11:49:48 +0200

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simple
+{
+    public partial class Form1 : Form
+    {
+        double operand1, operand2, result;
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            result = operand1 - operand2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            result = operand1 * operand2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            result = operand1 / operand2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(txtOperand1.Text);
+            operand2 = Convert.ToDouble(txtOperand2.Text);
+            result = operand1 + operand2;
+            txtResult.Text = result.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
